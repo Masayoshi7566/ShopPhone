1e800e29b69f7caa19a067f7e97958a9eae4851f
Orathai <orathai.u@g-able.com>
2019-12-20 14:20:58 +0700

    Initial Commit

---
+++ ShopPhone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopPhone
+//
+//  Created by GABLET04690 on 20/12/2562 BE.
+//  Copyright © 2562 Orathai Udomluk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
